59264a2f3fb2c1246d25411b1ca3ea5255e54093
Santiago-CT <santiagocardonatovar@gmail.com>
2025-08-20 21:46:23 -0500

    Primer commit: proyecto Java

---
+++ demo/src/main/java/com/example/Main.java
@@ -0,0 +1,88 @@
+package com.example;
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
+
+class Persona{
+    private double ID;
+    private String nombres;
+    private String apellidos;
+    private String email;
+
+    public Persona(double ID, String nombres, String apellidos, String email){
+        this.ID = ID;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.email = email;
+    }
+    @Override
+    public String toString(){
+        return "ID: " + ID + ", Nombres: " + nombres + ", Apellidos: " + apellidos + ", Email: " + email;
+    }
+}
+
+class Profesor extends Persona{
+    private String tipoContrato;
+    public Profesor(double ID, String nombres, String apellidos, String email, String tipoContrato){
+        super(ID, nombres, apellidos, email);
+        this.tipoContrato = tipoContrato;
+    }
+    @Override
+    public String toString(){
+        return super.toString() + ", Tipo de contrato: " + tipoContrato;
+    }
+ }
+  
+class Estudiante extends Persona{
+    private double codigo;
+    private Programa programa;
+    private boolean activo;
+    private boolean promedio;
+    public Estudiante(double ID, String nombres, String apellidos, String email, double codigo, Programa programa, boolean activo, boolean promedio){
+        super(ID, nombres, apellidos, email);
+        this.codigo = codigo;
+        this.programa = programa;
+        this.activo = activo;
+        this.promedio = promedio;
+    }
+    @Override
+    public String toString(){
+        return super.toString() + ", Código: " + codigo + ", Programa: " +programa + ", Activo: " + activo + ", Promedio: " + promedio;
+}}
+class Programa {
+    private double ID;
+    private String nombre;
+    private double duracion;
+    private Date registro;
+    private Facultad facultad;
+    public Programa(double id,String nombre, double duracion, Date registro, Facultad facultad){
+        this.ID=ID;
+        this.nombre = nombre;
+        this.duracion = duracion;
+        this.registro = registro;
+        this.facultad = facultad;
+    }
+    @Override
+    public String toString(){
+        return "";
+    }
+}
+class Facultad{
+    private double ID;
+    private String nombre;
+    private Persona decano;
+    public Facultad(double id, String nombres,Persona decano){
+        this.ID = id;
+        this.nombre = nombre;
+        this.decano = decano;
+    }
+    @Override
+    public String toString(){
+        return "";
+    }
+}
+
